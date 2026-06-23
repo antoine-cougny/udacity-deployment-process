21570a08a5fab627c2583c39c98d23417ec30e61
TheLostWanderer <git@thelostwanderer.eu>
2026-06-23 23:32:33 +0200

    update set_env.sh

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=udacity-db.clics6suo8j9.eu-central-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
-export AWS_REGION=us-east-1
+export AWS_BUCKET=arn:aws:s3:::udagram-api-dev-1239810238102
+export AWS_REGION=eu-central-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
