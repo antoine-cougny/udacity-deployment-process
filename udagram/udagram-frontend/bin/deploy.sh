1e63bd2c16ab43af1ebc3b11b8616698456bad5f
TheLostWanderer <git@thelostwanderer.eu>
2026-06-24 14:49:37 +0200

    avoid hard-coding the S3 bucket name in bin/deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,7 @@
-aws s3 cp --recursive --acl public-read ./www s3://udagram-api-dev-123412431234213/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-api-dev-123412431234213/
+#!/usr/bin/env bash
+set -euo pipefail
+
+: "${BUCKET_NAME:?BUCKET_NAME environment variable is required}"
+
+aws s3 cp --recursive --acl public-read ./www "s3://${BUCKET_NAME}/"
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html "s3://${BUCKET_NAME}/"
