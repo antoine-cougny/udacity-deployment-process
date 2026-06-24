b2ff31e0a2bbae83d736be9b1ea0317ee3376399
TheLostWanderer <git@thelostwanderer.eu>
2026-06-24 14:55:47 +0200

    doc. add example env file to be sourced

---
+++ udagram/set_env.example.sh
@@ -0,0 +1,8 @@
+export POSTGRES_USERNAME=<your_postgres_username>
+export POSTGRES_PASSWORD=<your_postgres_password>
+export POSTGRES_HOST=<your_rds_host>
+export POSTGRES_DB=<your_database_name>
+export AWS_BUCKET=<your_bucket_name>
+export AWS_REGION=<your_region>
+export JWT_SECRET=<your_jwt_secret>
+export URL=<your_frontend_url>
